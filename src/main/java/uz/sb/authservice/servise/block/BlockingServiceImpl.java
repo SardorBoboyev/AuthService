1f8423dfff566caca95de1c84937f674362d6454
SardorBoboyev <boboyevsardor350@gmail.com>
2024-10-09 11:31:04 +0500

    Auth changed

--- src/main/java/uz/sb/authservice/servise/block/BlockingServiceImpl.java
+++ src/main/java/uz/sb/authservice/servise/block/BlockingServiceImpl.java
@@ -2,10 +2,10 @@ package uz.sb.authservice.servise.block;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import uz.sb.authservice.domain.dto.request.BlockingRequest;
 import uz.sb.authservice.domain.entity.BlockingEntity;
 import uz.sb.authservice.repository.AuthRepository;
 import uz.sb.authservice.repository.BlockingRepository;
+import uz.sb.domain.dto.request.BlockingRequest;
 
 
 @Service
